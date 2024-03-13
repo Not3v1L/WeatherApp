95e8bcbf8d6326eb6a2191a190d6740f7c89883b
Klim <bearshunter321@gmail.com>
2024-03-13 10:45:02 +0300

    Russian language changes

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
-        PORT = 5555
+        PORT = 5555 
 
     @bottle.route('/static/<filepath:path>')
     def server_static(filepath):
